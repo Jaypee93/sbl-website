5665e191af0f554183513550abd65a9d19beded3
Lexlinkan <alexander-lindqvist@live.se>
2023-11-08 22:27:03 +0100

    small updates

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import './Cards.css'
 
+import gh from "../assets/GH.png"
+
 const Cards = () => {
 
 
@@ -10,10 +12,29 @@ const Cards = () => {
             <div className="playerOfTheWeek">
                 <div className="powTitle">Player of the Week!</div>
                 <div className="powImage">
-                    
+                    <img src={gh} alt="GustavHansson" />
                 </div>
-                <div className="powStats"></div>
-                <div className="powName">ska ha en länk till spelarprofil</div>
+
+                <div className="powStats">
+                    <div className="pts">
+                        <div>PTS</div>
+                        <div>24</div>
+                    </div>
+                    <div className="ass">
+                        <div>ASS</div>
+                        <div>8</div>
+                    </div>
+                    <div className="reb">
+                        <div>REB</div>
+                        <div>2</div>
+                    </div>
+                    <div className="eff">
+                        <div>EFF</div>
+                        <div>21</div>
+                    </div>
+                </div>
+
+                <div className="powName"></div>
             </div>
         </div>
         <div className='Card'>
--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { useState, useEffect } from "react";
 import axios from "axios";
 import "./Homepage.css";
@@ -6,16 +5,17 @@ import Navbar from "./Navbar";
 import Navbarteams from "./NavbarTeams";
 import TeamPage from "./TeamPage";
 import Cards from "./Cards";
+import alex from "../assets/alexsbl2.png";
 
-const Homepage= ()  => {
-
+const Homepage = () => {
   return (
-        <div className="homepage-container">
-          <TeamPage/>
-                <Navbar/>
-                <Navbarteams />
-                <Cards />
-          {/* <div className="leagues-container">
+    <div className="homepage-container">
+      <TeamPage />
+      <Navbar />
+      <Navbarteams />
+      <img style={{ width: "100%", minHeight: "100%" }} src={alex} alt="" />
+      <Cards />
+      {/* <div className="leagues-container">
             {leagues.map((league) => {
               if (league.id === 93) {
                 return (
@@ -32,9 +32,8 @@ const Homepage= ()  => {
               }
             })}
           </div> */}
-        </div>
-      );
-
-}
+    </div>
+  );
+};
 
 export default Homepage;
